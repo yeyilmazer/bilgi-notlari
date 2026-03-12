1d023731c970f9c9349fc59c28e7e5417811675a
yeyilmazer <yeyilmazer@gmail.com>
2026-03-13 02:21:25 +0300

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -14,8 +14,14 @@ export default async function LoginPage() {
     <div className="container" style={{ maxWidth: 520 }}>
       <div className="card" style={{ padding: 24 }}>
         <h1>Admin Girişi</h1>
-        <form action={loginAction} className="grid" style={{ gap: 12 }}>
-          <input className="input" type="email" name="email" placeholder="E-posta" />
+<form
+  action={async (formData) => {
+    "use server";
+    await loginAction(formData);
+  }}
+  className="grid"
+  style={{ gap: 12 }}
+>          <input className="input" type="email" name="email" placeholder="E-posta" />
           <input className="input" type="password" name="password" placeholder="Şifre" />
           <button className="btn btn-primary" type="submit">Giriş Yap</button>
         </form>

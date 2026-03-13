3c0412a6c1cf6e8ad6849c4bcf739dd9c75fa2b8
yeyilmazer <yeyilmazer@gmail.com>
2026-03-13 04:56:41 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typedRoutes: true
+  typedRoutes: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
